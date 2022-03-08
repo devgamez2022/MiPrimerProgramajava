2216954cf822400c6bbb9e7c7ebbe87f3847baa9
devgamez2022 <winmanuel09@gmail.com>
2022-03-08 15:13:41 -0600

    Clase java con variables

---
+++ src/Developer/miprimeraplicacionjava.java
@@ -0,0 +1,29 @@
+package Developer;
+
+public class miprimeraplicacionjava {
+    public static void main(String[] args) {
+        String nombreCompleto, direccion, estadoCivil;
+        int edad;
+        double gastosDiarios;
+        char genero;
+        boolean poseeVehiculo;
+        
+        nombreCompleto = "Camila Esperanza Rosales";
+        direccion = "Calle de armagura desvio la tristera";
+        estadoCivil = "Soltera";
+        edad = 30;
+        gastosDiarios = 34.45;
+        genero = 'F';
+        poseeVehiculo = false;
+        
+        System.out.println("Nombre completo: " + nombreCompleto);
+        System.out.println("Direccion      : " + direccion);
+        System.out.println("Estado civil   : " + estadoCivil);
+        System.out.println("Edad           : " + edad);
+        System.out.println("Gastos diarios : " + gastosDiarios);
+        System.out.println("Genero         : " + genero);
+        System.out.println("Posee vehículo : " + poseeVehiculo);
+        
+        
+    }
+}
