12769a9f5149cac3fc63aa9fbfac751f6a095935
devgamez2022 <winmanuel09@gmail.com>
2022-03-08 15:27:43 -0600

    Clase Java CalculoInteresSimple

--- src/Developer/miprimeraplicacionjava.java
+++ src/Developer/miprimeraplicacionjava.java
@@ -4,6 +4,8 @@ public class miprimeraplicacionjava {
     public static void main(String[] args) {
         String nombreCompleto, direccion, estadoCivil;
         int edad;
+        int edad1 = 34;
+        
         double gastosDiarios;
         char genero;
         boolean poseeVehiculo;
