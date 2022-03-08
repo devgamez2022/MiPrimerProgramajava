81d22f4f1ebe04546e977b9378020bb1a552f947
devgamez2022 <winmanuel09@gmail.com>
2022-03-08 14:43:07 -0600

    Proyecto Java con NetBeans

---
+++ src/Developer/Variables.java
@@ -0,0 +1,17 @@
+package Developer;
+
+public class Variables {
+    public static void main(String[] args) {
+        
+        sumar();
+        
+    }
+
+    private static void sumar() {
+        int suma, a, b;
+        a = 12;
+        b = 30;
+        suma = a + b;
+        System.out.println("La suma es:  " + suma);
+    }
+}
